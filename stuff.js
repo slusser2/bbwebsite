2eec842939dad9b9ed92f57e30194617ff7700b1
slusser2 <slusser2@pdx.edu>
2017-06-08 14:10:14 -0700

    Added interaction with mockbox+

--- stuff.js
+++ stuff.js
@@ -1,19 +1,25 @@
 $('.enter').click(function(event){
-  $('.enter').hide()
   $('#agebox').addClass('active') // show
   $('#Age').change(function(event){
+    console.log('hi Anna!')
     $('#agebox').removeClass('active') // hide
-
-    var insults = {
-      '1-17': 'Do your parents know where you are, ya whipper snapper?!',
-      '18-22': 'Ooh! Barely legal!',
-      '23-29': 'Let’s get wriggety wrecked, son!',
-      '30-39': 'Don\'t you have kids to pick up from soccer?',
-      '40-99': 'Grab your bifocals, and let\'s get funky!'
-    }
-
-    $('#mockbox')
-      .html(insults[ $('#Age').val() ]) // get the insult form the object, based on the Age key
-      .addClass('active') // show
+    $('#mockbox').addClass('active') // show
+$('#mockbox').on('click', function(){
+     window.location = "page2.html";    
+});
   })
-})
+});
+
+  
+
+/*
+1-17= Do your parents know where you are, ya whipper snapper?! 
+
+18-22= Ooh! Barely legal! 
+
+23-29= Let’s get wriggety wrecked, son! 
+
+30-39= Don’t you have kids to pick up from soccer? 
+
+40-99= Grab your bifocals, and let’s get funky! 
+*/
